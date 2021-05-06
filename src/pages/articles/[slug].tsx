71f7157f3a9503e1b3493e0cf3bba99c640d0486
Marcelo Batista_CONTR <C0608083@vale.com>
2021-05-06 13:23:29 -0300

    Change revalidation time to 8 hours

--- src/pages/articles/[slug].tsx
+++ src/pages/articles/[slug].tsx
@@ -66,6 +66,6 @@ export const getStaticProps: GetStaticProps = async (content) => {
     props: {
       article: article,
     },
-    revalidate: 60 * 60 * 24,
+    revalidate: 60 * 60 * 8,
   };
 };
